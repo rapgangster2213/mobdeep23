d144626db8e075f6b003409106520a661bc93674
rapgangster2213 <148687749+rapgangster2213@users.noreply.github.com>
2023-10-23 17:57:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type Calculation struct {
@@ -80,15 +81,44 @@ func calcHandler(w http.ResponseWriter, r *http.Request) {
 		if !ok {
 			return
 		}
-		newExpression, err := govaluate.NewEvaluableExpression(expression)
+		if checkString(expression) {
+			newExpression, err := govaluate.NewEvaluableExpression(expression)
+			if err != nil {
+				result, err := calculateExpression(expression)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+				session.Values["result"] = result
+			} else {
+				result, err := newExpression.Evaluate(nil)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusBadRequest)
+					return
+				}
+				session.Values["result"] = result
+			}
+		} else {
+			http.Error(w, "Wrong expression", http.StatusBadRequest)
+			return
+		}
+		/*newExpression, err := govaluate.NewEvaluableExpression(expression)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 		result, err := newExpression.Evaluate(nil)
-		session.Values["result"] = result
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}*/
 		untypedResult, ok := session.Values["result"]
 		if !ok {
 			return
 		}
+		result, ok := untypedResult.(float64)
+		session.Values["result"] = result
 		resultOutput := untypedResult.(float64)
-
 		calculations = append(calculations, Calculation{
 			Expression: expression,
 			Result:     resultOutput,
@@ -96,12 +126,82 @@ func calcHandler(w http.ResponseWriter, r *http.Request) {
 		})
 
 		fmt.Fprintf(w, "<p>Result: %s<p>", strconv.FormatFloat(resultOutput, 'f', -1, 64)) // result output
-		templates.Execute(w, calculations)
+		err = templates.Execute(w, calculations)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
 		for _, calc := range calculations {
 			if username == calc.Session {
-				fmt.Fprintf(w, "<p>%s = %s", calc.Expression, strconv.FormatFloat(calc.Result, 'f', -1, 64))              // calc history output
-				fmt.Printf("%s = %s, key: %s\n", expression, strconv.FormatFloat(calc.Result, 'f', -1, 64), calc.Session) // bebra
+				fmt.Fprintf(w, "<p>%s = %s", calc.Expression, strconv.FormatFloat(calc.Result, 'f', -1, 64))                   // calc history output
+				fmt.Printf("%s = %s, key: %s\n", calc.Expression, strconv.FormatFloat(calc.Result, 'f', -1, 64), calc.Session) // bebra
 			}
 		}
 	}
+}
+
+func calculateExpression(expression string) (float64, error) {
+	expression = strings.ReplaceAll(expression, " ", "")
+	expression = strings.ReplaceAll(expression, ",", ".")
+
+	result, err := eval(expression)
+	if err != nil {
+		return 0, err
+	}
+
+	return result, nil
+}
+
+func eval(expression string) (float64, error) {
+	result, err := strconv.ParseFloat(expression, 64)
+	if err == nil {
+		return result, nil
+	}
+
+	operations := []string{"+", "/", "*", "-"}
+
+	for _, op := range operations {
+		if strings.Contains(expression, op) {
+			parts := strings.Split(expression, op)
+			if len(parts) != 2 {
+				return 0, fmt.Errorf("Invalid expression")
+			}
+
+			left, err := eval(parts[0])
+			if err != nil {
+				return 0, err
+			}
+
+			right, err := eval(parts[1])
+			if err != nil {
+				return 0, err
+			}
+
+			switch op {
+			case "+":
+				return left + right, nil
+			case "-":
+				return left - right, nil
+			case "*":
+				return left * right, nil
+			case "/":
+				if right == 0 {
+					return 0, fmt.Errorf("Division by zero")
+				}
+				return left / right, nil
+			}
+		}
+	}
+
+	return 0, fmt.Errorf("Invalid expression")
+}
+
+func checkString(s string) bool {
+	checkCharacters := []string{"(", ")", "-", "+", "*", "/", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	for _, c := range checkCharacters {
+		if strings.Contains(s, c) {
+			return true
+		}
+	}
+	return false
 }
